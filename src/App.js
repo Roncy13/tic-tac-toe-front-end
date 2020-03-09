cf4ce30eeaae5961f396d678f9c9cde9273de9ab
James Roncesvalles <roncy.pro13@gmail.com>
2020-03-09 10:25:52 +0800

    POC for the tic tac toe

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import io from 'socket.io-client';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  socket = {};
+  game = {};
+  room = "room-sample";
+
+  constructor() {
+    super();
+  
+    this.initialize = this.initialize.bind(this);
+    this.addNumber = this.addNumber.bind(this);
+    this.receivedPlacement = this.receivedPlacement.bind(this);
+    this.joinRoom = this.joinRoom.bind(this);
+
+    this.initialize(); 
+    this.receivedPlacement();
+  }
+
+  initialize() {
+    this.sendRoomValue = this.sendRoomValue.bind(this);
+    
+    this.socket = io.connect("http://localhost:4001/game");
+    this.socket.on('connect', () => { 
+      console.log("Connected Successfully");
+    });
+  }
+
+  receivedPlacement() {
+    this.socket.on("result", (payload) => {
+      console.log("result", payload)
+    });
+
+    this.socket.on("receivedRoom", (payload) => {
+      console.log("receivedRoom", payload);
+    });
+  }
+
+  sendRoomValue() {
+    this.socket.emit("createdRoom", { room: this.room });
+  }
+
+  addNumber({ target: { value } }) {
+    this.room = value;
+  }
+
+  joinRoom() {
+    this.socket.emit("joinRoom", { room: this.room });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        Type here:
+        <input type = "text" onChange = { this.addNumber } ></input>
+        <button onClick = { this.sendRoomValue }>Send room</button>
+        <button onClick = { this.joinRoom }>Join room</button>
+      </div>
+    );
+  } 
 }
-
-export default App;
